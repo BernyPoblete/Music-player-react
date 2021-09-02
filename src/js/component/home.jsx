8016fd04cd52f60846d07ebf3b125b98d5e7cbc4
BernyPoblete <btapoblete@gmail.com>
2021-09-02 19:41:30 +0000

    explicacion Victor

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,15 +1,45 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Navbar from "./navbar.jsx";
 import Playlist from "./playlist.jsx";
 import Footer from "./footer.jsx";
 
 //create your first component
 function Home() {
+	let [currentSong, setCurrentSong] = useState(null);
+	let audioPlayer = useRef(null);
+	const baseUrl = "https://assets.breatheco.de/apis/sound/";
+	const songs = [
+		{
+			id: 1,
+			category: "game",
+			name: "Mario Castle",
+			url: "files/mario/songs/castle.mp3"
+		},
+		{
+			id: 2,
+			category: "game",
+			name: "Mario Star",
+			url: "files/mario/songs/hurry-starman.mp3"
+		},
+		{
+			id: 3,
+			category: "game",
+			name: "Mario Overworld",
+			url: "files/mario/songs/overworld.mp3"
+		}
+	];
+
+	const setSong = url => {
+		audioPlayer.current.src = baseUrl + url;
+		audioPlayer.current.play();
+	};
+
 	return (
 		<div className="container">
 			<Navbar />
-			<Playlist />
+			<Playlist songsList={songs} setSong={setSong} />
 			<Footer />
+			<audio ref={audioPlayer} />
 		</div>
 	);
 }
--- src/js/component/playlist.jsx
+++ src/js/component/playlist.jsx
@@ -1,33 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Playlist() {
+function Playlist(props) {
+	let { songsList } = props;
 	return (
 		<div className="reproductor">
-			<div className="one">1</div>
-			<div className="two">2</div>
-			<div className="three">3</div>
-			<div className="four">4</div>
-			<div className="five">5</div>
-			<div className="six">6</div>
-			<div className="seven">7</div>
-			<div className="eight">8</div>
-			<div className="nine">9</div>
-			<div className="ten">10</div>
+			{songsList.map((song, index) => {
+				return (
+					<div key={index} onClick={() => props.setSong(song.url)}>
+						{`${index + 1} ${song.name}`}
+					</div>
+				);
+			})}
 		</div>
 	);
 }
 
 Playlist.propTypes = {
-	1: PropTypes.string,
-	2: PropTypes.string,
-	3: PropTypes.string,
-	4: PropTypes.string,
-	5: PropTypes.string,
-	6: PropTypes.string,
-	7: PropTypes.string,
-	8: PropTypes.string,
-	9: PropTypes.string,
-	10: PropTypes.string
+	songsList: PropTypes.array,
+	setSong: PropTypes.func
 };
 export default Playlist;
